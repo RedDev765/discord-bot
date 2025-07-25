f9926b4188a97e0069a5b07373812b6de8f3f701
RedDev765 <132697534+RedDev765@users.noreply.github.com>
2025-07-26 00:06:03 +0100

    Delete bot-discordd/bot-discordd directory

--- bot-discordd/bot-discordd/commands/lock.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "lock",
-  async execute(message) {
-    if (!message.member.permissions.has("ManageChannels")) return message.channel.send("❌ Permission refusée.");
-    await message.channel.permissionOverwrites.edit(message.guild.id, { SendMessages: false });
-    message.channel.send("🔒 Ce salon est maintenant verrouillé.");
-  }
-};
